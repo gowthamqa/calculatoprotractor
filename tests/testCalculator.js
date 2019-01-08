57fcdb62523ccc548e2ade377f5b1803990efbec
gowthamqa <mgowthamreddy.qa@gmail.com>
2019-01-08 11:36:45 +0530

    CI run

--- tests/testCalculator.js
+++ tests/testCalculator.js
@@ -19,6 +19,7 @@ describe('testing calculator',function(){
             calc.goButton.click();
             expect(calc.result()).toEqual(data.result);   
 			
+			
         });
             
     });
